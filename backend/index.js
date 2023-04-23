6e524d84f75b61359bb3bcc29c64856404124daf
Adityaweb3 <ap7596112@gmail.com>
2023-04-24 02:42:22 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,44 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/getTokens", async (req, res) => {
-  return res.status(200).json({});
+
+  const {userAddress, chain} = req.query ;
+  const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
+    chain : chain,
+    address : userAddress
+  }
+  );
+
+  const nfts  = await Moralis.EvmApi.nft.getWalletNFTs({
+    chain : chain,
+    address : userAddress,
+    mediaItems : true
+  }
+
+  );
+
+  const balance = await Moralis.EvmApi.balance.getNativeBalance({
+    chain: chain,
+    address: userAddress
+  });
+
+  const myNfts = nfts.raw.result.map((e, i) => {
+    if (e?.media?.media_collection?.high?.url && !e.possible_spam && (e?.media?.category !== "video") ) {
+      return e["media"]["media_collection"]["high"]["url"];
+    }
+  })
+
+  const jsonResponse = {
+    tokens : tokens.raw,
+    nfts : myNfts,
+    balance : balance.raw.balance / 10 ** 18
+
+  }
+
+
+
+
+  return res.status(200).json(jsonResponse);
 });
 
 Moralis.start({
--- mwallet/src/chains.js
+++ mwallet/src/chains.js
@@ -1,14 +1,14 @@
 const Ethereum = {
     hex: '0x1',
     name: 'Ethereum',
-    rpcUrl: '',
+    rpcUrl: 'https://mainnet.infura.io/v3/7351aa9731464a5ab4ff2bcd8701c450',
     ticker: "ETH"
 };
 
 const MumbaiTestnet = {
     hex: '0x13881',
     name: 'Mumbai Testnet',
-    rpcUrl: '',
+    rpcUrl: 'https://polygon-mumbai.blockpi.network/v1/rpc/public',
     ticker: "MATIC"
 };
 
